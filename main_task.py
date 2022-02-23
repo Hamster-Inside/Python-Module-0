1c39cb65dc83c65487bba9d98e01b41bc5e10992
Hamster-Inside <arkadiusz.budkowski.dev@gmail.com>
2022-02-23 21:59:11 +0100

    new def of hits

--- main_task.py
+++ main_task.py
@@ -6,7 +6,7 @@ import typing
 def number_table_generator(number: int = 10) -> typing.List[int]:
     """ Generate table of numbers
     :param number: 10
-    :return: {1,2,3,4,5,6,7,8,9,10}
+    :return: [1,2,3,4,5,6,7,8,9,10]
     """
     number_table = []
     for i in range(number):
@@ -14,21 +14,27 @@ def number_table_generator(number: int = 10) -> typing.List[int]:
     return number_table
 
 
-def random_number_table_generator(numbers: typing.List[int], table_of_numbers: typing.List[int]) -> typing.List[int]:
+def random_number_table_generator(table_of_numbers: typing.List[int]) -> typing.List[int]:
     """ Generator of 6 random numbers to be compared with given numbers
     :param table_of_numbers: [1,2,4,5,...,49] -> or any other table to be mixed
-    :param numbers: [4,8,12,5,10,22] -> numbers we want to bet
-    :return: number of the same numbers from given nums and lotto machine
+    :return: [3,9,1,2,10,33] -> random number list of 6
     """
     randomly_mixed_numbers = []
     for _ in range(6):
         random_number = random.choice(table_of_numbers)
         randomly_mixed_numbers.append(random_number)
         table_of_numbers.remove(random_number)
-    numbers_hit = len((set(numbers)) & (set(randomly_mixed_numbers)))
-    return numbers_hit
+    return randomly_mixed_numbers
+
 
 def winning_numbers_counter(picked_numbers: typing.List[int], random_numbers: typing.List[int]) -> int:
+    """ Returns number of numbers that are the same in random number list
+    :param picked_numbers: [1,3,22,11,38,40]
+    :param random_numbers: [38,22,5,7,8,9]
+    :return: 2
+    """
+    numbers_hit = len((set(picked_numbers)) & (set(random_numbers)))
+    return numbers_hit
 
 
 how_many_times_played = 0
@@ -38,7 +44,8 @@ base_number_table = number_table_generator(49)
 
 while True:
     how_many_times_played += 1
-    if random_number_table_generator(bet_numbers, base_number_table.copy()) == 6:
+    random_lotto_numbers = random_number_table_generator(base_number_table)
+    if winning_numbers_counter(bet_numbers, random_lotto_numbers) == 6:
         break
 
 sum_of_money_spent = cost_of_one_bet * how_many_times_played
--- main_task_test.py
+++ main_task_test.py
@@ -1,6 +1,13 @@
 from main_task import number_table_generator
+from main_task import winning_numbers_counter
 
 
 def test_generating_number_table():
     table = number_table_generator(5)
     assert table == [1, 2, 3, 4, 5]
+
+
+def test_winning_numbers_counter_01():
+    my_table = [1, 2, 3, 4, 5, 6]
+    random_table = [2, 4, 6, 33, 22, 18]
+    assert test_winning_numbers_counter(my_table, random_table) == 3
